f56b952d2719f4f72d43fecc71d6108962869aec
umeadi_i <israelumeadi@westat.com>
2019-04-23 14:54:52 -0400

    Package Updated

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ const chartDemoData = [
   {'createdGroup': 'Alliance', 'accrualCnt': 55},
   {'createdGroup': 'Swog', 'accrualCnt': 22},
   {'createdGroup': 'CEPT', 'accrualCnt': 91}
-];
+]
 
 export default class App extends Component {
   render () {
